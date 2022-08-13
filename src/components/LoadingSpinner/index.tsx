a00df091ad3ea6e2e3f1c7a15de2c905f8b529d7
skymins04 <skymin0417@gmail.com>
2022-08-13 12:12:42 +0900

    LoadingSpinner component 수정

--- src/components/LoadingSpinner/index.tsx
+++ src/components/LoadingSpinner/index.tsx
@@ -1,7 +1,7 @@
 const LoadingSpinner = () => {
   return (
     <img
-      style={{ display: "block", margin: "auto" }}
+      style={{ display: "block", width: "50%", margin: "auto" }}
       src="https://cdn.jsdelivr.net/gh/skymins04/real-time-river-level-korea/public/loading-spin.gif"
       alt=""
     />
