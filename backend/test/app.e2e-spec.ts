6c959d07e46a459a17a2d836d8bf7eea0ec694d9
skymins04 <skymin0417@gmail.com>
2022-08-24 19:44:31 +0900

    test coverage 환경 설정 추가

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -15,10 +15,7 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/test (GET)', () => {
+    return request(app.getHttpServer()).get('/test').expect(401);
   });
 });
