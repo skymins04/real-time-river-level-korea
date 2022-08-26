81124612f31de7d1d93e14817d1fb577c20d34bc
skymins04 <skymin0417@gmail.com>
2022-08-27 05:14:45 +0900

    DB에 등록된 관측소 및 관측데이터를 삭제 할 때 캐싱된 데이터와 타이밍을 맞추기 위해 crawling service에 삭제관련 메서드 추가 & crawling service의 주석 추가 & crawling service의 테스트 코드 추가

--- backend/src/crawling/crawling.service.spec.ts
+++ backend/src/crawling/crawling.service.spec.ts
@@ -6,6 +6,7 @@ import {
   CreateCrawlingRivergaugeBodyDTO,
   UpdateCrawlingRivergaugeBodyDTO,
 } from './dto/crawling-rivergauge-body.dto';
+import { CreateCrawlingRiverlevelBodyDTO } from './dto/crawling-riverlevel-body.dto';
 
 describe('CrawlingService', () => {
   let crawlingService: CrawlingService;
@@ -60,6 +61,30 @@ describe('CrawlingService', () => {
         },
       ],
     });
+    await prismaService.riverlevel_water_level_tb.createMany({
+      data: [
+        {
+          obscd: 'test_1_cd',
+          obsdate: '2022-08-27T04:33:00Z',
+          water_level: 1,
+        },
+        {
+          obscd: 'test_2_cd',
+          obsdate: '2022-08-27T04:33:00Z',
+          water_level: 1,
+        },
+        {
+          obscd: 'test_3_cd',
+          obsdate: '2022-08-27T04:33:00Z',
+          water_level: 1,
+        },
+        {
+          obscd: 'test_4_cd',
+          obsdate: '2022-08-27T04:33:00Z',
+          water_level: 1,
+        },
+      ],
+    });
   };
 
   beforeAll(async () => {
@@ -73,17 +98,7 @@ describe('CrawlingService', () => {
   });
 
   afterEach(async () => {
-    try {
-      await prismaService.riverlevel_water_level_tb.deleteMany({
-        where: { obscd: { startsWith: 'test' } },
-      });
-      await prismaService.riverlevel_gauge_tb.deleteMany({
-        where: { obscd: { startsWith: 'test' } },
-      });
-    } catch (e) {
-      console.log(e);
-    }
-    crawlingService.clearGauges();
+    await crawlingService.deleteManyGauge('test');
   });
 
   it('should be defined', () => {
@@ -94,27 +109,6 @@ describe('CrawlingService', () => {
   describe('initGauges()', () => {
     it('should be defined', () => {
       expect(crawlingService.initGauges).toBeDefined();
-    });
-  });
-
-  describe('clearGauges()', () => {
-    it('shoud be defined', () => {
-      expect(crawlingService.clearGauges).toBeDefined();
-    });
-  });
-
-  describe('getRegistedGaguges()', () => {
-    it('should be defined', () => {
-      expect(crawlingService.getRegistedGauges).toBeDefined();
-    });
-
-    it('should be return registed gauges', async () => {
-      await initTestData();
-      const { gauges, gaugeCodes } = await crawlingService.getRegistedGauges();
-      expect(gauges).toBeDefined();
-      expect(gauges.map((x) => x.obscd).includes('test_1_cd')).toBe(true);
-      expect(gaugeCodes).toBeDefined();
-      expect(gaugeCodes.includes('test_1_cd')).toBe(true);
     });
   });
 
@@ -140,6 +134,122 @@ describe('CrawlingService', () => {
     });
   });
 
+  describe('deleteOneGauge()', () => {
+    it('should be defined', () => {
+      expect(crawlingService.deleteOneGauge).toBeDefined();
+    });
+
+    it('should be delete one gauge', async () => {
+      await initTestData();
+      await crawlingService.initGauges(true);
+      await crawlingService.deleteOneGauge('test_1_cd');
+      expect(crawlingService.getOneRegistedGauges('test_1_cd')).toBeUndefined();
+    });
+
+    it('should not be delete one gauge', async () => {
+      await initTestData();
+      await crawlingService.initGauges(true);
+      await crawlingService.deleteOneGauge('');
+      expect(crawlingService.getOneRegistedGauges('test_1_cd')).toBeDefined();
+      expect(crawlingService.getOneRegistedGauges('test_2_cd')).toBeDefined();
+      expect(crawlingService.getOneRegistedGauges('test_3_cd')).toBeDefined();
+      expect(crawlingService.getOneRegistedGauges('test_4_cd')).toBeDefined();
+    });
+  });
+
+  describe('deleteManyGauge()', () => {
+    it('should be defined', () => {
+      expect(crawlingService.deleteManyGauge).toBeDefined();
+    });
+
+    it('should be delete all gauges that obscd start with "test"', async () => {
+      await initTestData();
+      await crawlingService.initGauges(true);
+      await crawlingService.deleteManyGauge('test');
+      expect(
+        (
+          await prismaService.riverlevel_gauge_tb.findMany({
+            where: { obscd: { startsWith: 'test' } },
+          })
+        ).length,
+      ).toBe(0);
+      expect(
+        (
+          await prismaService.riverlevel_water_level_tb.findMany({
+            where: { obscd: { startsWith: 'test' } },
+          })
+        ).length,
+      ).toBe(0);
+    });
+  });
+
+  describe('deleteOneRiverlevel()', () => {
+    it('should be defined', () => {
+      expect(crawlingService.deleteOneRiverlevel).toBeDefined();
+    });
+
+    it('should be delete one riverlevel', async () => {
+      await initTestData();
+      await crawlingService.initGauges(true);
+      await crawlingService.deleteOneRiverlevel(
+        'test_1_cd',
+        '2022-08-27T04:33:00Z',
+      );
+      expect(
+        (
+          await prismaService.riverlevel_water_level_tb.findMany({
+            where: {
+              obscd: 'test_1_cd',
+              obsdate: new Date('2022-08-27T04:33:00Z'),
+            },
+          })
+        ).length,
+      ).toBe(0);
+    });
+  });
+
+  describe('deleteManySpecificGaugeRiverlevel()', () => {
+    it('should be defined', () => {
+      expect(crawlingService.deleteManySpecificGaugeRiverlevel).toBeDefined();
+    });
+
+    it('should be delete all riverlevels of gauge that obscd is "test_1_cd"', async () => {
+      await initTestData();
+      await crawlingService.initGauges(true);
+      await crawlingService.deleteManySpecificGaugeRiverlevel('test_1_cd');
+      expect(
+        (
+          await prismaService.riverlevel_water_level_tb.findMany({
+            where: {
+              obscd: 'test_1_cd',
+            },
+          })
+        ).length,
+      ).toBe(0);
+    });
+  });
+
+  describe('deleteManyRiverlevel()', () => {
+    it('should be defined', () => {
+      expect(crawlingService.deleteManyRiverlevel).toBeDefined();
+    });
+
+    it('should be delete all riverlevels of gauges that obscd start with "test"', async () => {
+      await initTestData();
+      await crawlingService.initGauges(true);
+      await crawlingService.deleteManyRiverlevel('test');
+      expect(
+        (
+          await prismaService.riverlevel_water_level_tb.findMany({
+            where: {
+              obscd: { startsWith: 'test' },
+            },
+          })
+        ).length,
+      ).toBe(0);
+    });
+  });
+
   describe('setRivergauge()', () => {
     it('should be defined', () => {
       expect(crawlingService.setRivergauge).toBeDefined();
@@ -149,8 +259,8 @@ describe('CrawlingService', () => {
       const body: CreateCrawlingRivergaugeBodyDTO = {
         data: [
           {
-            obscd: 'test_5_cd',
-            obsnm: 'test_5_name',
+            obscd: 'test_1_cd',
+            obsnm: 'test_1_name',
             mngorg: 'test_org',
             flood_warning: 'N',
             addr: 'test addr',
@@ -160,8 +270,8 @@ describe('CrawlingService', () => {
             planflood_level: null,
           },
           {
-            obscd: 'test_6_cd',
-            obsnm: 'test_6_name',
+            obscd: 'test_2_cd',
+            obsnm: 'test_2_name',
             mngorg: 'test_org',
             flood_warning: 'N',
             addr: 'test addr',
@@ -171,8 +281,8 @@ describe('CrawlingService', () => {
             planflood_level: null,
           },
           {
-            obscd: 'test_7_cd',
-            obsnm: 'test_7_name',
+            obscd: 'test_3_cd',
+            obsnm: 'test_3_name',
             mngorg: 'test_org',
             flood_warning: 'N',
             addr: 'test addr',
@@ -187,17 +297,17 @@ describe('CrawlingService', () => {
       expect(result.status).toBe('Success');
       expect(result.requested).toBe(3);
       expect(result.processed).toBe(3);
-      expect(crawlingService.getOneRegistedGauges('test_5_cd')).toBeDefined();
-      expect(crawlingService.getOneRegistedGauges('test_6_cd')).toBeDefined();
-      expect(crawlingService.getOneRegistedGauges('test_7_cd')).toBeDefined();
+      expect(crawlingService.getOneRegistedGauges('test_1_cd')).toBeDefined();
+      expect(crawlingService.getOneRegistedGauges('test_2_cd')).toBeDefined();
+      expect(crawlingService.getOneRegistedGauges('test_3_cd')).toBeDefined();
     });
 
     it('should be return {status: "Success", requested: 3, processed: 0}', async () => {
       const body: CreateCrawlingRivergaugeBodyDTO = {
         data: [
           {
-            obscd: 'test_8_cd',
-            obsnm: 'test_8_name',
+            obscd: 'test_1_cd',
+            obsnm: 'test_1_name',
             mngorg: 'test_org',
             flood_warning: 'N',
             addr: 'test addr',
@@ -207,8 +317,8 @@ describe('CrawlingService', () => {
             planflood_level: null,
           },
           {
-            obscd: 'test_9_cd',
-            obsnm: 'test_9_name',
+            obscd: 'test_2_cd',
+            obsnm: 'test_2_name',
             mngorg: 'test_org',
             flood_warning: 'N',
             addr: 'test addr',
@@ -218,8 +328,8 @@ describe('CrawlingService', () => {
             planflood_level: null,
           },
           {
-            obscd: 'test_10_cd',
-            obsnm: 'test_10_name',
+            obscd: 'test_3_cd',
+            obsnm: 'test_3_name',
             mngorg: 'test_org',
             flood_warning: 'N',
             addr: 'test addr',
@@ -235,17 +345,17 @@ describe('CrawlingService', () => {
       expect(result.status).toBe('Success');
       expect(result.requested).toBe(3);
       expect(result.processed).toBe(0);
-      expect(crawlingService.getOneRegistedGauges('test_8_cd')).toBeDefined();
-      expect(crawlingService.getOneRegistedGauges('test_9_cd')).toBeDefined();
-      expect(crawlingService.getOneRegistedGauges('test_10_cd')).toBeDefined();
+      expect(crawlingService.getOneRegistedGauges('test_1_cd')).toBeDefined();
+      expect(crawlingService.getOneRegistedGauges('test_2_cd')).toBeDefined();
+      expect(crawlingService.getOneRegistedGauges('test_3_cd')).toBeDefined();
     });
 
     it('should be filter duplicate itmes', async () => {
       const body: CreateCrawlingRivergaugeBodyDTO = {
         data: [
           {
-            obscd: 'test_11_cd',
-            obsnm: 'test_11_name',
+            obscd: 'test_1_cd',
+            obsnm: 'test_1_name',
             mngorg: 'test_org',
             flood_warning: 'N',
             addr: 'test addr',
@@ -255,8 +365,8 @@ describe('CrawlingService', () => {
             planflood_level: null,
           },
           {
-            obscd: 'test_11_cd',
-            obsnm: 'test_11_name',
+            obscd: 'test_1_cd',
+            obsnm: 'test_1_name',
             mngorg: 'test_org',
             flood_warning: 'N',
             addr: 'test addr',
@@ -266,8 +376,8 @@ describe('CrawlingService', () => {
             planflood_level: null,
           },
           {
-            obscd: 'test_11_cd',
-            obsnm: 'test_11_name',
+            obscd: 'test_1_cd',
+            obsnm: 'test_1_name',
             mngorg: 'test_org',
             flood_warning: 'N',
             addr: 'test addr',
@@ -282,9 +392,9 @@ describe('CrawlingService', () => {
       expect(result.status).toBe('Success');
       expect(result.requested).toBe(3);
       expect(result.processed).toBe(1);
-      expect(crawlingService.getOneRegistedGauges('test_11_cd')).toBeDefined();
-      expect(crawlingService.getOneRegistedGauges('test_11_cd').obsnm).toBe(
-        'test_11_name',
+      expect(crawlingService.getOneRegistedGauges('test_1_cd')).toBeDefined();
+      expect(crawlingService.getOneRegistedGauges('test_1_cd').obsnm).toBe(
+        'test_1_name',
       );
     });
   });
@@ -298,8 +408,8 @@ describe('CrawlingService', () => {
       const createBody: CreateCrawlingRivergaugeBodyDTO = {
         data: [
           {
-            obscd: 'test_14_cd',
-            obsnm: 'test_14_name',
+            obscd: 'test_1_cd',
+            obsnm: 'test_1_name',
             mngorg: 'test_org',
             flood_warning: 'N',
             addr: 'test addr',
@@ -309,8 +419,8 @@ describe('CrawlingService', () => {
             planflood_level: null,
           },
           {
-            obscd: 'test_15_cd',
-            obsnm: 'test_15_name',
+            obscd: 'test_2_cd',
+            obsnm: 'test_2_name',
             mngorg: 'test_org',
             flood_warning: 'N',
             addr: 'test addr',
@@ -320,8 +430,8 @@ describe('CrawlingService', () => {
             planflood_level: null,
           },
           {
-            obscd: 'test_16_cd',
-            obsnm: 'test_16_name',
+            obscd: 'test_3_cd',
+            obsnm: 'test_3_name',
             mngorg: 'test_org',
             flood_warning: 'N',
             addr: 'test addr',
@@ -334,23 +444,23 @@ describe('CrawlingService', () => {
       };
       await crawlingService.setRivergauge(createBody);
 
-      expect(crawlingService.getOneRegistedGauges('test_14_cd')).toBeDefined();
-      expect(crawlingService.getOneRegistedGauges('test_15_cd')).toBeDefined();
-      expect(crawlingService.getOneRegistedGauges('test_16_cd')).toBeDefined();
+      expect(crawlingService.getOneRegistedGauges('test_1_cd')).toBeDefined();
+      expect(crawlingService.getOneRegistedGauges('test_2_cd')).toBeDefined();
+      expect(crawlingService.getOneRegistedGauges('test_3_cd')).toBeDefined();
 
       const updateBody: UpdateCrawlingRivergaugeBodyDTO = {
         data: [
           {
-            obscd: 'test_14_cd',
+            obscd: 'test_1_cd',
             addr: 'updated test addr',
           },
           {
-            obscd: 'test_15_cd',
+            obscd: 'test_2_cd',
             lon: '222-222-222',
             lat: '222-222-222',
           },
           {
-            obscd: 'test_16_cd',
+            obscd: 'test_3_cd',
             flood_warning: 'Y',
           },
         ],
@@ -360,15 +470,175 @@ describe('CrawlingService', () => {
       expect(result.status).toBe('Success');
       expect(result.requested).toBe(3);
       expect(result.processed).toBe(3);
-      expect(crawlingService.getOneRegistedGauges('test_14_cd').addr).toBe(
+      expect(crawlingService.getOneRegistedGauges('test_1_cd').addr).toBe(
         'updated test addr',
       );
-      expect(crawlingService.getOneRegistedGauges('test_15_cd').lon).toBe(
+      expect(crawlingService.getOneRegistedGauges('test_2_cd').lon).toBe(
         '222-222-222',
       );
       expect(
-        crawlingService.getOneRegistedGauges('test_16_cd').flood_warning,
+        crawlingService.getOneRegistedGauges('test_3_cd').flood_warning,
       ).toBe('Y');
+    });
+  });
+
+  describe('setRiverlevel()', () => {
+    it('should be defined', () => {
+      expect(crawlingService.setRiverlevel).toBeDefined();
+    });
+
+    it('should be return {status: "Success", requested: 3, processed: 3}', async () => {
+      const rivergaugeBody: CreateCrawlingRivergaugeBodyDTO = {
+        data: [
+          {
+            obscd: 'test_1_cd',
+            obsnm: 'test_1_name',
+            mngorg: 'test_org',
+            flood_warning: 'N',
+            addr: 'test addr',
+            lon: '1-1-1',
+            lat: '1-1-1',
+            gdt: null,
+            planflood_level: null,
+          },
+          {
+            obscd: 'test_2_cd',
+            obsnm: 'test_2_name',
+            mngorg: 'test_org',
+            flood_warning: 'N',
+            addr: 'test addr',
+            lon: '2-2-2',
+            lat: '2-2-2',
+            gdt: null,
+            planflood_level: null,
+          },
+          {
+            obscd: 'test_3_cd',
+            obsnm: 'test_3_name',
+            mngorg: 'test_org',
+            flood_warning: 'N',
+            addr: 'test addr',
+            lon: '3-3-3',
+            lat: '3-3-3',
+            gdt: null,
+            planflood_level: null,
+          },
+        ],
+      };
+      const riverlevelBody: CreateCrawlingRiverlevelBodyDTO = {
+        data: [
+          {
+            obscd: 'test_1_cd',
+            obsdate: '2022-08-27T02:01:00Z',
+            water_level: 1,
+          },
+          {
+            obscd: 'test_2_cd',
+            obsdate: '2022-08-27T02:01:00Z',
+            water_level: 1,
+          },
+          {
+            obscd: 'test_3_cd',
+            obsdate: '2022-08-27T02:01:00Z',
+            water_level: 1,
+          },
+        ],
+      };
+      await crawlingService.setRivergauge(rivergaugeBody);
+      const res = await crawlingService.setRiverlevel(riverlevelBody);
+
+      expect(res.status).toBe('Success');
+      expect(res.requested).toBe(3);
+      expect(res.processed).toBe(3);
+    });
+
+    it('should be return {status: "Success", requested: 3, processed: 0}', async () => {
+      const rivergaugeBody: CreateCrawlingRivergaugeBodyDTO = {
+        data: [
+          {
+            obscd: 'test_1_cd',
+            obsnm: 'test_1_name',
+            mngorg: 'test_org',
+            flood_warning: 'N',
+            addr: 'test addr',
+            lon: '1-1-1',
+            lat: '1-1-1',
+            gdt: null,
+            planflood_level: null,
+          },
+          {
+            obscd: 'test_2_cd',
+            obsnm: 'test_2_name',
+            mngorg: 'test_org',
+            flood_warning: 'N',
+            addr: 'test addr',
+            lon: '2-2-2',
+            lat: '2-2-2',
+            gdt: null,
+            planflood_level: null,
+          },
+          {
+            obscd: 'test_3_cd',
+            obsnm: 'test_3_name',
+            mngorg: 'test_org',
+            flood_warning: 'N',
+            addr: 'test addr',
+            lon: '3-3-3',
+            lat: '3-3-3',
+            gdt: null,
+            planflood_level: null,
+          },
+        ],
+      };
+      const riverlevelBody1: CreateCrawlingRiverlevelBodyDTO = {
+        data: [
+          {
+            obscd: 'test_1_cd',
+            obsdate: '2022-08-27T02:01:00Z',
+            water_level: 1,
+          },
+          {
+            obscd: 'test_2_cd',
+            obsdate: '2022-08-27T02:01:00Z',
+            water_level: 1,
+          },
+          {
+            obscd: 'test_3_cd',
+            obsdate: '2022-08-27T02:01:00Z',
+            water_level: 1,
+          },
+        ],
+      };
+      const riverlevelBody2: CreateCrawlingRiverlevelBodyDTO = {
+        data: [
+          {
+            obscd: 'test_1_cd',
+            obsdate: '2022-08-27T02:01:00Z',
+            water_level: 1,
+          },
+          {
+            obscd: 'test_2_cd',
+            obsdate: '2022-08-27T02:01:00Z',
+            water_level: 1,
+          },
+          {
+            obscd: 'test_4_cd',
+            obsdate: '2022-08-27T02:01:00Z',
+            water_level: 1,
+          },
+        ],
+      };
+      await crawlingService.setRivergauge(rivergaugeBody);
+      await crawlingService.setRiverlevel(riverlevelBody1);
+      const res1 = await crawlingService.setRiverlevel(riverlevelBody1);
+      const res2 = await crawlingService.setRiverlevel(riverlevelBody2);
+
+      expect(res1.status).toBe('Success');
+      expect(res1.requested).toBe(3);
+      expect(res1.processed).toBe(0);
+      expect(res2.status).toBe('Success');
+      expect(res2.requested).toBe(3);
+      expect(res2.processed).toBe(0);
     });
   });
 });
--- backend/src/crawling/crawling.service.ts
+++ backend/src/crawling/crawling.service.ts
@@ -16,45 +16,197 @@ import DBCache from 'src/lib/db-cache';
 export class CrawlingService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  private registedGaugeCodes: string[];
-  private registedGauges: riverlevel_gauge_tb[];
-  private registedRiverlevel: RiverLevels;
+  private registedGaugeCodes: string[] = [];
+  private registedGauges: riverlevel_gauge_tb[] = [];
+  private registedRiverlevel: RiverLevels = {};
   private dbCache: DBCache;
 
-  async getRegistedGauges() {
-    const gauges = await this.prismaService.riverlevel_gauge_tb.findMany();
-    const gaugeCodes = gauges.map((obs) => obs.obscd);
-    return { gauges, gaugeCodes };
+  /**
+   * DB에 등록된 하천수위 관측소들의 관측소 코드들 중 특정 문자열로 시작되는 관측소 코드들을 반환하는 함수
+   * @param obscdStartWidth 하천수위 관측소 코드의 시작 문자열
+   * @returns 특정 문자열로 시작되는 관측소 코드들
+   */
+  private _getGaugeCodesStartWith(obscdStartWidth: string) {
+    return this.registedGaugeCodes.filter((x) => {
+      return new RegExp(`^${obscdStartWidth}`).test(x);
+    });
   }
 
+  /**
+   * DB에 등록된 하천수위 관측소등 중 특정 관측소 코드를 가진 하천수위 관측소의 객체를 반환하는 함수
+   * @param obscd 하천수위 관측소 코드
+   * @returns 특정 관측소 코드의 하천수위 관측소 객체
+   */
   getOneRegistedGauges(obscd) {
     return this.registedGauges.filter((obs) => obs.obscd === obscd)[0];
   }
 
+  /**
+   * DB에 등록된 특정 관측소 코드를 가진 하천수위 관측소 하나를 제거하는 함수
+   * 삭제될 하천수위 관측소의 하천수위 관측데이터들도 모두 제거된다.
+   * @param obscd 하천수위 관측소 코드
+   */
+  async deleteOneGauge(obscd: string) {
+    if (this.registedGaugeCodes.includes(obscd)) {
+      await this.deleteManySpecificGaugeRiverlevel(obscd);
+      await this.prismaService.riverlevel_gauge_tb.delete({ where: { obscd } });
+      this.registedGaugeCodes = this.registedGaugeCodes.filter(
+        (x) => x !== obscd,
+      );
+      this.registedGauges = this.registedGauges.filter(
+        (x) => x.obscd !== obscd,
+      );
+    }
+  }
+
+  /**
+   * DB에 등록된 하천수위 관측소들 중 특정 문자열로 시작하는 관측소 코드를 가진 하천수위 관측소들을 모두 제거하는 함수
+   * 삭제될 하천수위 관측소들의 하천수위 관측데이터들도 모두 제거된다.
+   * @param obscdStartWith 하천수위 관측소 코드의 시작 문자열
+   */
+  async deleteManyGauge(obscdStartWith: string) {
+    const deleteTargetGaugeCodes = this._getGaugeCodesStartWith(obscdStartWith);
+    await this.prismaService.riverlevel_water_level_tb.deleteMany({
+      where: { obscd: { in: deleteTargetGaugeCodes } },
+    });
+    await this.prismaService.riverlevel_gauge_tb.deleteMany({
+      where: { obscd: { in: deleteTargetGaugeCodes } },
+    });
+
+    this.registedGaugeCodes = this.registedGaugeCodes.filter(
+      (x) => !deleteTargetGaugeCodes.includes(x),
+    );
+    this.registedGauges = this.registedGauges.filter(
+      (x) => !deleteTargetGaugeCodes.includes(x.obscd),
+    );
+    for (const obscd of deleteTargetGaugeCodes) {
+      if (this.registedRiverlevel[obscd]) delete this.registedRiverlevel[obscd];
+    }
+  }
+
+  /**
+   * 특정 하천수위 관측 데이터를 제거하는 함수
+   * @param obscd 하천수위 관측소 코드
+   * @param obsdate 하천수위 관측 일자
+   */
+  async deleteOneRiverlevel(obscd: string, obsdate: string) {
+    if (
+      this.registedRiverlevel[obscd] &&
+      this.registedRiverlevel[obscd].includes(obsdate)
+    ) {
+      await this.prismaService.riverlevel_water_level_tb.deleteMany({
+        where: { obscd: obscd, obsdate: new Date(obsdate) },
+      });
+      this.registedRiverlevel[obscd] = this.registedRiverlevel[obscd].filter(
+        (x) => x !== obsdate,
+      );
+    }
+  }
+
+  /**
+   * 특정 하천수위 관측소의 모든 하천수위 관측 데이터들을 제거하는 함수
+   * @param obscd 하천수위 관측소 코드
+   */
+  async deleteManySpecificGaugeRiverlevel(obscd: string) {
+    if (
+      this.registedRiverlevel[obscd] &&
+      this.registedRiverlevel[obscd].length > 0
+    ) {
+      await this.prismaService.riverlevel_water_level_tb.deleteMany({
+        where: {
+          obscd,
+        },
+      });
+      delete this.registedRiverlevel[obscd];
+    }
+  }
+
+  /**
+   * 특정 문자열로 시작하는 관측소 코드의 하천수위 관측소의 모든 데이터를 제거하는 함수
+   * @param obscdStartWith 하천수위 관측소의 시작 문자열
+   */
+  async deleteManyRiverlevel(obscdStartWith: string) {
+    const deleteTargetGaugeCodes = this._getGaugeCodesStartWith(obscdStartWith);
+    for (const obscd of deleteTargetGaugeCodes) {
+      if (this.registedRiverlevel[obscd]) delete this.registedRiverlevel[obscd];
+    }
+    await this.prismaService.riverlevel_water_level_tb.deleteMany({
+      where: { obscd: { in: deleteTargetGaugeCodes } },
+    });
+  }
+
+  /**
+   * DB 캐싱 객체에서 내부 필드로 하천수위 관측소들과 하천수위 관측 데이터를 가져오는 함수
+   * 기본적으로 내부 필드 dhCache가 할당되지 않았을때만 실행됨.
+   * force에 true를 대입하면 강제적으로 실행됨.
+   * @param force 강제 초기화 실행 플래그
+   */
   async initGauges(force = false) {
     if (!this.dbCache || force) {
       this.dbCache = await DBCache.getInstance();
+      if (force) await this.dbCache.initCache();
       this.registedGauges = this.dbCache.getRegistedRivergauge();
       this.registedGaugeCodes = this.dbCache.getRegistedRivergaugeCode();
       this.registedRiverlevel = this.dbCache.getRegistedRiverlevel();
     }
   }
 
-  clearGauges() {
-    this.dbCache = null;
-    this.registedGauges = [];
-    this.registedGaugeCodes = [];
-    this.registedRiverlevel = {};
-  }
-
+  /**
+   * [POST] /crawling/riverlevel 엔드포인트의 서비스 함수
+   * @param body post body
+   * @returns 요청 결과
+   */
   async setRiverlevel(body: CreateCrawlingRiverlevelBodyDTO) {
     await this.initGauges();
 
-    const requestedObsKeys: string[] = [];
+    const requestedObsKeys: RiverLevels = {};
+    const riverlevelRaw = body.data.filter((obs) => {
+      if (
+        !this.registedRiverlevel[obs.obscd] ||
+        !this.registedRiverlevel[obs.obscd].includes(obs.obsdate)
+      )
+        if (this.registedGaugeCodes.includes(obs.obscd)) {
+          if (!requestedObsKeys[obs.obscd]) requestedObsKeys[obs.obscd] = [];
 
-    return true;
+          if (!requestedObsKeys[obs.obscd].includes(obs.obsdate)) {
+            requestedObsKeys[obs.obscd].push(obs.obsdate);
+            return true;
+          }
+        }
+      return false;
+    });
+    const riverlevelArray: riverlevel_water_level_tb[] = riverlevelRaw.map(
+      (riverlevel) => ({
+        no: undefined,
+        obscd: riverlevel.obscd,
+        obsdate: new Date(riverlevel.obsdate),
+        water_level: riverlevel.water_level,
+      }),
+    );
+
+    if (riverlevelArray.length !== 0) {
+      for (const riverlevel of riverlevelRaw) {
+        if (!this.registedRiverlevel[riverlevel.obscd])
+          this.registedRiverlevel[riverlevel.obscd] = [];
+        this.registedRiverlevel[riverlevel.obscd].push(riverlevel.obsdate);
+      }
+      await this.prismaService.riverlevel_water_level_tb.createMany({
+        data: riverlevelArray,
+      });
+    }
+
+    return {
+      status: 'Success',
+      requested: body.data.length,
+      processed: riverlevelArray.length,
+    };
   }
 
+  /**
+   * [POST] /crawling/rivergauge 엔드포인트의 서비스 함수
+   * @param body post body
+   * @returns 요청 결과
+   */
   async setRivergauge(body: CreateCrawlingRivergaugeBodyDTO) {
     await this.initGauges();
 
@@ -96,6 +248,11 @@ export class CrawlingService {
     };
   }
 
+  /**
+   * [PATCH] /crawling/rivergauge 엔드포인트의 서비스 함수
+   * @param body post body
+   * @returns 요청 결과
+   */
   async updateRivergauge(body: UpdateCrawlingRivergaugeBodyDTO) {
     await this.initGauges();
 
